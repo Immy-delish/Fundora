0806ce39b746579ddfb24e4c3000ca1a139b03e6
Immaculate Atim <immaculateatim56@gmail.com>
2025-01-15 16:46:56 +0300

    Added new pages

--- loan_management/views.py
+++ loan_management/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+def sign_in(request):
+    return render(request, 'sign_in.html')  # Renders the Sign In page
+
+def dashboard(request):
+    return render(request, 'dashboard.html')  # Renders the Dashboard page
+
+def add_client(request):
+    return render(request, 'add_client.html')  # Renders the Add New Client page
